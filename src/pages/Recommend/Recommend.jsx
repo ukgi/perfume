6ccce7bd6dedd945a8fdd52920b41684107b6c36
unknown <doosan1224@naver.com>
2023-03-26 18:12:32 +0900

    [!HOTFIX] :로그아웃 시, 추천 페이지 라우팅 금지
    access token이 없을 시, 추천인 페이지에 라우팅되지 않도록 수정

--- src/pages/Recommend/Recommend.jsx
+++ src/pages/Recommend/Recommend.jsx
@@ -50,38 +50,44 @@ export default function Recommend() {
   };
 
   return (
-    <div className={styles.container}>
-      <div className={styles.userInfo}>
-        <img
-          className={styles.userImage}
-          src={sessionStorage.getItem("thumbnailImage")}
-          alt=''
-        />
-        <h2>{nickName}</h2>
-        <h3>향수 설문 데이터</h3>
-        <div>
-          <input
-            className={styles.input}
-            type='text'
-            ref={copyLinkRef}
-            defaultValue={`https://inhyang.netlify.app/recommend/${nickName}/${id}`}
-          />
-          <button className={styles.linkBtn} onClick={copyTextUrl}>
-            링크 복사
+    <>
+      {sessionStorage.getItem("accessToken") ? (
+        <div className={styles.container}>
+          <div className={styles.userInfo}>
+            <img
+              className={styles.userImage}
+              src={sessionStorage.getItem("thumbnailImage")}
+              alt=''
+            />
+            <h2>{nickName}</h2>
+            <h3>향수 설문 데이터</h3>
+            <div>
+              <input
+                className={styles.input}
+                type='text'
+                ref={copyLinkRef}
+                defaultValue={`https://inhyang.netlify.app/recommend/${nickName}/${id}`}
+              />
+              <button className={styles.linkBtn} onClick={copyTextUrl}>
+                링크 복사
+              </button>
+            </div>
+          </div>
+          <div className={styles.testContainer}>
+            <h3 className={styles.testContainerTitle}>실시간 테스트 현황</h3>
+            <div className={styles.testResultBox}>
+              <p className={styles.testTakers}>{recommendData.length}명</p>
+              <p className={styles.testTakersDesc}>응시자 수</p>
+            </div>
+          </div>
+          <BestRecommend />
+          <button onClick={handleRecommendDetail} className={styles.button}>
+            자세히 알아보기
           </button>
         </div>
-      </div>
-      <div className={styles.testContainer}>
-        <h3 className={styles.testContainerTitle}>실시간 테스트 현황</h3>
-        <div className={styles.testResultBox}>
-          <p className={styles.testTakers}>{recommendData.length}명</p>
-          <p className={styles.testTakersDesc}>응시자 수</p>
-        </div>
-      </div>
-      <BestRecommend />
-      <button onClick={handleRecommendDetail} className={styles.button}>
-        자세히 알아보기
-      </button>
-    </div>
+      ) : (
+        <div>해당 사이트에 접속하실 수 없습니다❌</div>
+      )}
+    </>
   );
 }
