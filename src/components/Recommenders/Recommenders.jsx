d786394d6c205247bceb9579a0227e81c310c598
unknown <doosan1224@naver.com>
2023-05-10 21:53:41 +0900

    [Design] : 세부 디자인 조정

--- src/components/Recommenders/Recommenders.jsx
+++ src/components/Recommenders/Recommenders.jsx
@@ -1,4 +1,3 @@
-// import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import React from "react";
 import { useState } from "react";
@@ -9,18 +8,6 @@ import { config as server } from "../../config";
 export default function Recommenders() {
   const id = sessionStorage.getItem("id");
   const [recommenders, setRecommenders] = useState();
-
-  // const { data: recommenders } = useQuery(["recommenders"], async () => {
-  //   const accessToken = sessionStorage.getItem("accessToken");
-  //   const config = {
-  //     headers: { Authorization: `${accessToken}` },
-  //   };
-  //   const data = await axios.get(
-  //     `${process.env.REACT_APP_SERVER_DOMAIN}/member/show-recommended-perfume/${id}`,
-  //     config
-  //   );
-  //   return data.data.recommendationList;
-  // });
 
   useEffect(() => {
     const handleRecommenders = async () => {
