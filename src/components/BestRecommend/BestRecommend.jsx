b4441a884a4770242c55d8e6fc64a6018209e75d
unknown <doosan1224@naver.com>
2023-05-15 19:17:45 +0900

    [fix] : 디자인 수정

--- src/components/BestRecommend/BestRecommend.jsx
+++ src/components/BestRecommend/BestRecommend.jsx
@@ -91,7 +91,7 @@ export default function BestRecommend() {
               ? Object.keys(bestRecommend)[1]
               : "추천된 향수가 없습니다"}
           </h3>
-          {bestRecommend ? (
+          {Object.keys(bestRecommend)[1] ? (
             <img
               className={styles.bestPerfume}
               src={`/assets/images/perfumeImg/${
