19e41e0fa49cf5cdc8d0c5df0d1616a7956f16ff
unknown <doosan1224@naver.com>
2023-05-20 18:01:43 +0900

    [fix] : 세부 기능 수정

--- src/pages/Recommend/Recommend.jsx
+++ src/pages/Recommend/Recommend.jsx
@@ -100,6 +100,7 @@ export default function Recommend() {
             width: "100%",
             height: "100vh",
             display: "flex",
+            flexDirection: "column",
             alignItems: "center",
             justifyContent: "center",
           }}
