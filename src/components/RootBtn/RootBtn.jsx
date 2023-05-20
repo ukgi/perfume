8e974806a312870b2a815cff741c67e6c4898f64
unknown <doosan1224@naver.com>
2023-05-20 17:59:23 +0900

    [fix] : 세부 기능 수정

--- src/components/RootBtn/RootBtn.jsx
+++ src/components/RootBtn/RootBtn.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import styles from "./RootBtn.module.css";
 import { useNavigate } from "react-router-dom";
 
-export default function RootBtn() {
+export default function RootBtn({ option }) {
   const navigate = useNavigate();
   return (
-    <button className={styles.rootBtn} onClick={() => navigate("/services")}>
+    <button
+      className={
+        option === "brandName" ? styles.rootBtnBrandName : styles.rootBtn
+      }
+      onClick={() => navigate("/services")}
+    >
       처음으로
     </button>
   );
