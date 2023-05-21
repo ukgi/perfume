72ffad803ac2c982d97cd5e0631f535e6006f52e
unknown <doosan1224@naver.com>
2023-05-21 23:21:16 +0900

    [Fix] : BestRecommend 오류 fix

--- src/components/BestRecommend/BestRecommend.jsx
+++ src/components/BestRecommend/BestRecommend.jsx
@@ -1,66 +1,65 @@
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import React from "react";
-import { useState } from "react";
-import { useEffect } from "react";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { config as server } from "../../config";
 
-// import styles from "./BestRecommend.module.css";
+import styles from "./BestRecommend.module.css";
 
 export default function BestRecommend() {
   const id = sessionStorage.getItem("id");
-  const [bestRecommend, setBestRecommend] = useState({});
-  const [bestPerfume, setBestPerfume] = useState();
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
-  useEffect(() => {
-    const accessToken = sessionStorage.getItem("accessToken");
-    const config = {
-      headers: { Authorization: `${accessToken}` },
-    };
-    const handleBestRecommend = async () => {
-      try {
-        const data = await axios.get(
-          `${server.api}/show-analyzed-data/${id}`,
-          config
-        );
-        console.log(data.data);
-        console.log(bestRecommend);
-        setBestRecommend(data.data);
-        const bestPerfumeImage = await axios({
-          method: "get",
-          url: `${server.api}/perfume/perfume-image`,
-          headers: { Authorization: `${accessToken}` },
-          params: {
-            perfumeName: `${Object.keys(data.data)[0]}`,
-          },
-        });
-        console.log(bestPerfume);
-        setBestPerfume(bestPerfumeImage.data);
-        return data.data;
-      } catch (err) {
-        console.log(err);
-        if (err.response.status === 404) {
-          console.log("추천 목록이 없습니다 ❌");
-        }
+  const handleDetailPerfume = () =>
+    navigate(`/brandDetail/${bestPerfume.id}`, { state: bestPerfume });
+
+  const { data: bestRecommend } = useQuery(["bestRecommend"], async () => {
+    try {
+      const accessToken = sessionStorage.getItem("accessToken");
+      const config = {
+        headers: { Authorization: `${accessToken}` },
+      };
+      const data = await axios.get(
+        `${server.api}/show-analyzed-data/${id}`,
+        config
+      );
+
+      return data.data;
+    } catch (err) {
+      console.log(err);
+      if (err.response.status === 404) {
+        console.log("추천 목록이 없습니다 ❌");
       }
-    };
-    handleBestRecommend();
-  }, [id]);
+    }
+  });
 
-  // const handleDetailPerfume = () =>
-  //   navigate(`/brandDetail/${bestPerfume.id}`, { state: bestPerfume });
+  const { data: bestPerfume } = useQuery(["bestPerfume"], async () => {
+    try {
+      const accessToken = sessionStorage.getItem("accessToken");
+      const bestPerfumeImage = await axios({
+        method: "get",
+        url: `${server.api}/perfume/perfume-image`,
+        headers: { Authorization: `${accessToken}` },
+        params: {
+          perfumeName: `${bestRecommend.perfumeAnalyzeObject.perfumeName}`,
+        },
+      });
+      return bestPerfumeImage;
+    } catch (err) {
+      console.error(err);
+    }
+  });
 
   return (
     <>
-      {/* <div className={styles.testContainer}>
+      <div className={styles.testContainer}>
         <h2 className={styles.testContainerTitle}>Best 향수 추천</h2>
         <div className={styles.testResultBox}>
           <h3 className={styles.testTakers}>
             {bestRecommend ? bestRecommend.perfumeAnalyzeObject.count : 0}
           </h3>
           <h3 className={styles.testTakersDesc}>
-            {bestRecommend.perfumeAnalyzeObject.perfumeName ? (
+            {bestRecommend ? (
               bestRecommend.perfumeAnalyzeObject.perfumeName
             ) : (
               <p>추천 향수가 없습니다</p>
@@ -88,11 +87,11 @@ export default function BestRecommend() {
             {bestRecommend ? bestRecommend.scentAnalyzeObject.count : 0}
           </h3>
           <h3 className={styles.testTakersDesc}>
-            {bestRecommend.scentAnalyzeObject.scent
+            {bestRecommend
               ? bestRecommend.scentAnalyzeObject.scent
               : "추천 향기가 없습니다"}
           </h3>
-          {bestRecommend.scentAnalyzeObject.scent ? (
+          {bestRecommend ? (
             <img
               className={styles.bestPerfume}
               src={`/assets/images/perfumeImg/${bestRecommend.scentAnalyzeObject.scent}.webp`}
@@ -102,7 +101,7 @@ export default function BestRecommend() {
             <></>
           )}
         </div>
-      </div> */}
+      </div>
     </>
   );
 }
